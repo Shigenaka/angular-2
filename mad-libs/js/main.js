111acb5a4dc3db015f56982e803336a6fa5792f7
Shigenaka <mshig19@uw.edu>
2015-11-03 13:53:04 -0800

    finished mad-libs challenge

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,13 @@
 // Create application
-
+var madApp = angular.module('madApp', []);
 
 // Bind controller madCtrl
+madApp.controller('madCtrl', function($scope) {
 
-
-	// Set initial values for name, age, location, and food
+    // Set initial values for name, age, location, and food
+    $scope.name = 'Shig';
+    $scope.age = '20';
+    $scope.location = 'Seattle';
+    $scope.food = 'Sushi';
+})
 
