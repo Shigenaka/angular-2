0268a8608becccacedf8fde23c89581f5d27b78f
Shigenaka <mshig19@uw.edu>
2015-11-03 14:38:23 -0800

    completed js for data-request

--- data-request/js/main.js
+++ data-request/js/main.js
@@ -4,7 +4,7 @@ var dataApp = angular.module('dataApp', []);
 // Bind controller customersCtrl
 dataApp.controller('customersCtrl', function($scope, $http) {
     $http.get('http://www.w3schools.com/angular/customers.php').then(function successCallback(response) {
-        $scope.names = response.names;
-        console.log($scope.names);
+        $scope.names = request.data.records;
+        console.log(request.data);
     })
 })
