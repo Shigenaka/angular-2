b0185b5b82897dfa80fac2cef3f0bf683d84d029
Shigenaka <mshig19@uw.edu>
2015-11-03 14:15:16 -0800

    progress on data-request

--- data-request/js/main.js
+++ data-request/js/main.js
@@ -1,4 +1,10 @@
 // Create application
-
+var dataApp = angular.module('dataApp', []);
 
 // Bind controller customersCtrl
+dataApp.controller('customersCtrl', function($scope, $http) {
+    $http.get('http://www.w3schools.com/angular/customers.php').then(function successCallback(response) {
+        $scope.names = response.names;
+        console.log($scope.names);
+    })
+})
